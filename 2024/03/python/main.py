68110bf10a1d66655ffad7c9f58d868aefa94407
mhsn <mail@mhsn.net>
2024-12-03 12:00:00 +0000

    2024-03 python p1,p2

---
+++ 2024/03/python/main.py
@@ -0,0 +1,22 @@
+import re
+import sys
+
+with open(sys.argv[1], "r") as f:
+    s = f.read()
+
+silver = sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", s))
+
+gold = 0
+active = True
+for g in re.findall(r"(?:mul\((\d{1,3}),(\d{1,3})\))|(don't\(\))|(do\(\))", s):
+    match g, active:
+        case (x, y, "", ""), True:
+            gold += int(x) * int(y)
+        case (_, _, "don't()", _), _:
+            active = False
+        case (_, _, _, "do()"), _:
+            active = True
+
+
+print("silver:", silver)
+print("gold:", gold)

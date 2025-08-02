6f7c790cb1f1ec721b073c6f3b930f9054234822
mhsn <mail@mhsn.net>
2025-08-02 16:12:08 +0100

    2024-04 rust p2

--- 2024/04/rust/src/main.rs
+++ 2024/04/rust/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> io::Result<()> {
     let grid = Grid::new(grid);
 
     let silver: usize = silver(&grid);
-    let gold: u64 = 0;
+    let gold: usize = gold(&grid);
     println!("silver: {silver}");
     println!("gold: {gold}");
 
@@ -22,6 +22,25 @@ fn silver(grid: &Grid<char>) -> usize {
     grid.points()
         .flat_map(|p| chebyshev_ball(1).map(move |dp| grid.ray(p, dp)))
         .map(|cs| cs.take(4).eq_by("XMAS".chars(), |a, b| *a == b))
+        .filter(|p| *p)
+        .count()
+}
+
+fn gold(grid: &Grid<char>) -> usize {
+    let xmas: Vec<(i64, i64)> = vec![(0, 0), (-1, -1), (1, -1), (-1, 1), (1, 1)];
+    grid.points()
+        .map(move |p| {
+            xmas.iter()
+                .map(|&d| grid.move_pos(p, d.into(), 1).and_then(|p| grid.at(p)))
+                .collect::<Option<Vec<_>>>()
+        })
+        .flatten()
+        .map(|cs| {
+            vec!["AMSMS", "AMMSS", "ASMSM", "ASSMM"]
+                .iter()
+                .map(|x| cs.iter().eq_by(x.chars(), |&&a, b| a == b))
+                .any(|p| p)
+        })
         .filter(|p| *p)
         .count()
 }
